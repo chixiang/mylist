5591ef69f7d2ba6e7dcc3cfef840ea1166ea6da4
chixiang <chixiang1983@gmail.com>
2017-01-12 23:52:15 +0800

    list整理完成

--- mylist/AllListViewController.swift
+++ mylist/AllListViewController.swift
@@ -20,21 +20,18 @@ class AllListViewController: UITableViewController, listDetailDelegate {
         self.navigationItem.title = listCategory.name
     }
     
-//    func addList(list: List) {
-//        lists.append(list)
-//        let indexPath = NSIndexPath(row: lists.count - 1, section: 0)
-//        tableView.insertRows(at: [indexPath as IndexPath], with: UITableViewRowAnimation.automatic)
-//    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
     
-    // MARK: ListDetailDelegate
+    // MARK: - ListDetailDelegate
     func listDetailViewController(controller: ListDetailViewController, didFinishAddList list: List) {
         lists.append(list)
         let indexPath = NSIndexPath(row: lists.count - 1, section: 0)
         tableView.insertRows(at: [indexPath as IndexPath], with: UITableViewRowAnimation.automatic)
         saveAllList()
     }
-    
-
     
     func listDetailViewController(controller: ListDetailViewController, didFinishEditList list: List) {
         let index = lists.index(of: list)
@@ -46,69 +43,14 @@ class AllListViewController: UITableViewController, listDetailDelegate {
         }
         saveAllList()
     }
-
-    // MARK: UITableView
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return lists.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "listCell", for: indexPath) as! ListCell
-        
-        let list = lists[indexPath.row]
-        
-        configureCell(cell: cell, list: list)
-        
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        let list = lists[indexPath.row]
-        
-        let optionalCell = tableView.cellForRow(at: indexPath)
-        
-        if let cell = optionalCell {
-            let checkMarkLabel = cell.viewWithTag(1001) as! UILabel
-            if checkMarkLabel.text == "√" {
-                checkMarkLabel.text = ""
-                list.isChecked = false
-            } else {
-                checkMarkLabel.text = "√"
-                list.isChecked = true
-            }
-        }
-        saveAllList()
-    }
-    
-//    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-//        if indexPath.row != 0 {
-//            return true
-//        } else {
-//            return false
-//        }
-//    }
-    
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        lists.remove(at: indexPath.row)
-        tableView.deleteRows(at: [indexPath], with: .automatic)
-        saveAllList()
-    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let navigationVC = segue.destination as! UINavigationController
         let addListVC = navigationVC.topViewController as! ListDetailViewController
         addListVC.addListDelegate = self
         
-        
         if segue.identifier == "addList" {
-            //addListVC.allListVC = self
-            addListVC.addListDelegate = self
+            // TODO
         } else if segue.identifier == "editList" {
             let cell = sender as! UITableViewCell
             let indexPath = tableView.indexPath(for: cell)
@@ -120,22 +62,20 @@ class AllListViewController: UITableViewController, listDetailDelegate {
         }
     }
     
-    
-    func configureCell(cell: ListCell, list: List) {
-        cell.customLabel.text = list.name
-        
-        let checkMarkLabel = cell.viewWithTag(1001) as! UILabel
-        print(list.isChecked)
-        if list.isChecked {
-            //cell.accessoryType = .checkmark
-            checkMarkLabel.text = "√"
-        } else {
-            //cell.accessoryType = .none
-            checkMarkLabel.text = ""
-        }
-        
+    // MARK: - UITableViewDataSource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return lists.count
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listCell", for: indexPath) as! ListCell
+        let list = lists[indexPath.row]
+        
+        configureCell(cell: cell, list: list)
+        
+        return cell
+    }
+    
     func dataFilePath() -> String {
         let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
         let filePath = paths[0] as NSString
@@ -155,7 +95,7 @@ class AllListViewController: UITableViewController, listDetailDelegate {
         data.write(toFile: dataFilePath(), atomically: true)
         
     }
-
+    
     func loadAllList() {
         // 1. 创建 data 对象
         let data = NSData(contentsOfFile: dataFilePath())
@@ -165,6 +105,46 @@ class AllListViewController: UITableViewController, listDetailDelegate {
             lists = unarchiver.decodeObject(forKey: "AllList") as! [List]
             unarchiver.finishDecoding()
         }
+    }
+
+    // MARK: - 事件
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let list = lists[indexPath.row]
+        
+        let optionalCell = tableView.cellForRow(at: indexPath)
+        if let cell = optionalCell {
+            let checkMarkLabel = cell.viewWithTag(1001) as! UILabel
+            if checkMarkLabel.text == "√" {
+                checkMarkLabel.text = ""
+                list.isChecked = false
+            } else {
+                checkMarkLabel.text = "√"
+                list.isChecked = true
+            }
+        }
+        saveAllList()
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        lists.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+        saveAllList()
+    }
+    
+    func configureCell(cell: ListCell, list: List) {
+        cell.customLabel.text = list.name
+        
+        let checkMarkLabel = cell.viewWithTag(1001) as! UILabel
+        print(list.isChecked)
+        if list.isChecked {
+            //cell.accessoryType = .checkmark
+            checkMarkLabel.text = "√"
+        } else {
+            //cell.accessoryType = .none
+            checkMarkLabel.text = ""
+        }
+        
     }
 }
 
