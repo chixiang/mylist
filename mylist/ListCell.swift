0025d676f9581927d4608d7fa451d62a6e7c0be2
chixiang <chixiang1983@gmail.com>
2017-01-13 23:24:04 +0800

    完成代码整理

--- mylist/ListCell.swift
+++ mylist/ListCell.swift
@@ -13,13 +13,11 @@ class ListCell: UITableViewCell {
     @IBOutlet weak var customLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
 
 }
